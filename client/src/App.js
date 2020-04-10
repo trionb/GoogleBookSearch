abf43ee6ccbe82ab6d57569245c71f736c77f3e2
trion bailey <trionbailey@gmail.com>
2020-04-10 16:30:21 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "./components/Nav/Nav"
+
+function App () {
+    return (
+            <>
+            <Nav />
+            </>
+    );
+}
+export default App;
---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Jumbotron() {
+    return (
+        <div class="shadow-lg p-3 mb-5 rounded">
+            <div class="jumbotron1">
+                <div class="container">
+                    <h1 class="display-4">Google Books Search</h1>
+                    <p class="subHead">Search for and save books of interest.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Jumbotron;
